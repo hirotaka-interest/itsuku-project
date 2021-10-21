b7e98086650be195589ede9ce8760165efb09c63
Your Namegit config --global user.email arisa1213.aaa@gmail.comgit config --global user.name Arisa <you@example.com>
2021-10-21 18:41:13 +0900

    subimageとmainimageのクラス名を変更

--- js/script.js
+++ js/script.js
@@ -5,27 +5,22 @@ $(function () {
     activeClass: 'navi-active',//現在のページ番号に任意のclassを付与できます
     firstText: '<i class="fas fa-angle-double-left"></i>', // "最初ページ"に移動するfontawesome
     lastText: '<i class="fas fa-angle-double-right"></i>', // "最後のページ"に移動するfontawesome
-  })
+  });
 
 
 
   // -----サムネイル画像を押すと、メイン画像が変わる-----
-  $('.subimage li').hover(function(){
+  $('.shop__img-choice li').click(function(){
 
-    var index = $('.subimage li').index(this);
+    var index = $('.shop__img-choice li').index(this);
 
-    var imageurl = $('.subimage li').eq(index).find('img').attr('src');
+    var imageurl = $('.shop__img-choice li').eq(index).find('img').attr('src');
 
     //ulのクラス名を取得（空白で分割）
     className = $(this).parent().attr('class').split(" ");
 
-    //元のメイン画像を保存しておく
-    defaultImage = $('img.mainimage.'+className[1]).attr('src');
-
-    $('img.mainimage.'+className[1]).attr('src',imageurl);
-},function(){
-    $('img.mainimage.'+className[1]).attr('src',defaultImage);
-});
+    $('img.shop__img-gallery-image.'+className[1]).attr('src',imageurl);
+  });
 });
 
 
